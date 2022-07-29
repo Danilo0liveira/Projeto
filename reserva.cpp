d2ef62f9695eaf693233b146379bf6819483e735
Danilo0liveira <100158412+Danilo0liveira@users.noreply.github.com>
2022-07-29 19:50:22 -0300

    Add files via upload

--- reserva.cpp
+++ reserva.cpp
@@ -1,15 +1,19 @@
-#include "reserva.h"
+#include "reserva.h" //include do arquivo 'reserva.h'
 
-Reserva::Reserva(int& nm_acmp, int& nm_quarto, std::string& nome)
+//definição do constructor
+Reserva::Reserva(int& nm_acmp, int& nm_quarto, string& nome)
 {
     this->nm_acmp = (nm_acmp < 0) ? 0 : nm_acmp;
     this->nm_quarto = (nm_quarto < 0) ? 0 : nm_quarto;
     this->nome = nome;
     
-//     srand(unsigned time(0)); incluir na main
+    // srand(unsigned time(0)); incluir na main
+    //gera número aleatório para a reserva
     this->nm_res = 66666 + (rand() % 6666) + (rand() % 6666);
 }
 
+
+//definição dos métodos gets
 int Reserva::get_nmReserva() const
 {
     return nm_res;
@@ -20,7 +24,7 @@ int Reserva::get_nmAc() const
     return nm_acmp;
 }
 
-std::string Reserva::get_nome() const 
+string Reserva::get_nome() const 
 {
     return nome;
 }
--- reserva.h
+++ reserva.h
@@ -1,22 +1,25 @@
 #ifndef reserva_h
 #define reserva_h
 
-#include <iostream>
-#include <string>
-#include <cstdlib>
+//bibliotecas que serão incluidas na main
+// #include <iostream>
+// #include <string>
+// #include <cstdlib>
 
 class Reserva
 {
 private:
     int nm_res, nm_acmp, nm_quarto;
-    std::string nome;
+    string nome;
 
 public:
-    Reserva(int& nm_acmp, int& nm_quarto, std::string& nome);
+    // constructor
+    Reserva(int& nm_acmp, int& nm_quarto, string& nome);
+    //métodos gets
     int get_nmReserva() const;
     int get_nmAc() const;
     int get_nmquarto() const;
-    std::string get_nome() const;
+    string get_nome() const;
 
 };
 
