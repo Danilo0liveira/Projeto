bf4a2c4974b709fa519f56cc45ddc5016e688870
Danilo0liveira <100158412+Danilo0liveira@users.noreply.github.com>
2022-07-29 20:12:03 -0300

    Update QuartoPcD.cpp

--- QuartoPcD.cpp
+++ QuartoPcD.cpp
@@ -1,10 +1,12 @@
 #include "QuartoPcD.h"
 
+//constructor
 QuartoPcD::QuartoPcD(int numQuarto, int andar, int camas, int capacidade, float diaria, string tipoAdaptacao, int servicoEspecial): Quarto(numQuarto, andar, camas, capacidade, diaria){
     setTipoAdaptacao(tipoAdaptacao);
     setServ(servicoEspecial);
 }
 
+//métodos set (atributos da classe derivada)
 void QuartoPcD::setTipoAdaptacao(string tipoAdaptacao){
     this->tipoAdaptacao = tipoAdaptacao;
 }
@@ -13,6 +15,7 @@ void QuartoPcD::setServ(int servicoEspecial){
     this->servicoEspecial = servicoEspecial;
 }
 
+//metodos gets
 string QuartoPcD::getTipoAdaptacao() const{
     return tipoAdaptacao;
 }
