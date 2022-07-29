b154f6af741690f83d681f377ae91b4f69c5ebac
Danilo0liveira <100158412+Danilo0liveira@users.noreply.github.com>
2022-07-29 20:05:33 -0300

    Update QuartoPcD.h

--- QuartoPcD.h
+++ QuartoPcD.h
@@ -2,18 +2,24 @@
 #define QuartoPcD_H
 
 //Quarto vai ser incluido na main
-
 //supondo que using namespace std;
 
-class QuartoPcD:public Quarto{
+class QuartoPcD : public Quarto
+{
+// atributos privados
+private:
     string tipoAdaptacao;
     int servicoEspecial;
-    public:
-        QuartoPcD(int, int, int, int, float, string, int);
-        void setTipoAdaptacao(string);
-        void setServ(int);
-        string getTipoAdaptacao() const;
-        int getServ() const;
+public:
+// constructor
+    QuartoPcD(int, int, int, int, float, string, int);
+//  métodos sets
+    void setTipoAdaptacao(string);
+    void setServ(int);
+//  métodos gets
+    string getTipoAdaptacao() const;
+    int getServ() const;
+    
 };
 
 #endif
