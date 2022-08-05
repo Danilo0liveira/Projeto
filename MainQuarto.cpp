0675b39df0a64b1680559de127298b5c280491f2
gokutta8 <110000707+gokutta8@users.noreply.github.com>
2022-08-05 18:35:38 -0300

    Add files via upload

--- MainQuarto.cpp
+++ MainQuarto.cpp
@@ -1,16 +1,14 @@
-//int andar, int camas, int capacidade, float diaria, int numero
-
-
 #include <iostream>
 
 using namespace std;
 
-#include "quarto.cpp"
+#include "quarto.cpp"//Incluimos o quarto.cpp para testar o quarto.
 
 int main(){
-    Quarto quartoVazio;
-    Quarto quartoConstrutor(2, 4, 5, 250.25 , 240);
+    Quarto quartoVazio;//Criacao do objeto com construtor vazio.
+    Quarto quartoConstrutor(2, 4, 5, 250.25 , 240);//Criacao do objeto com construtor completo.
 
+    //Impressao completa do quartoVazio.
     cout << "Quarto vazio: " << endl << "Andar: " << quartoVazio.getAndar()
     << endl << "Camas: " << quartoVazio.getCamas() << endl << "Capacidade: "<< quartoVazio.getCapacidade()
     << endl << "Diaria: " << quartoVazio.getDiaria()
@@ -22,6 +20,7 @@ int main(){
     else
         cout << "Situacao: desocupado" << endl;
 
+    //Impressao completa do quartoConstrutor.
     cout << "Quarto construtor: " << endl << "Andar: " << quartoConstrutor.getAndar()
     << endl << "Camas: " << quartoConstrutor.getCamas() << endl << "Capacidade: " << quartoConstrutor.getCapacidade()
     << endl << "Diaria: " << quartoConstrutor.getDiaria()
@@ -33,9 +32,11 @@ int main(){
     else
         cout << "Situacao: desocupado" << endl;
 
+    //Mudanca da situacao dos quartos.
     quartoVazio.setSituacao(true);
     quartoConstrutor.setSituacao(true);
 
+    //Cout da mudanca dos quartos feita com sucesso.
     if(quartoVazio.getSituacao())
         cout << "Situacao: ocupado" << endl;
 
