163c1a157b78afd56d5dbde3a43fc9991aece944
gokutta8 <110000707+gokutta8@users.noreply.github.com>
2022-08-05 18:54:12 -0300

    Add files via upload

---
+++ MainReserva.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+#include "reserva.cpp"//Inclui-se o arquivo reserva.cpp.
+
+int main(){
+    Reserva reservaConstrutor(5, 200, "ANGELO");//Criado o objeto reserva com os construtores setados.
+
+    //Imprimido os valores de reserva.
+    cout << "Reserva:" << endl << "Numero de reserva: " << reservaConstrutor.get_nmReserva()
+    << endl << "Numero de acompanhantes: " << reservaConstrutor.get_nmAc()
+    << endl << "Numero do quarto: " << reservaConstrutor.get_nmquarto() << endl
+    << "Nome do cliente: " << reservaConstrutor.get_nome() << endl;
+
+    return 0;
+}
